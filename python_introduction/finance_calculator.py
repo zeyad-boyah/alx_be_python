6ca53a60b5fd2b8b9d886ad277c1c86f49ded08b
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2024-12-14 14:44:37 +0200

    adjusted the varible names from camelcase to snake case

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-monthlyIncome = int(input("Enter your monthly income: "))
-monthlyExpenses = int(input("Enter your total monthly expenses: "))
-monthlySavings = monthlyIncome - monthlyExpenses
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 # Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05) annual interest rate of 5%
-projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * 0.05)
-print(f"Your monthly savings are ${monthlySavings}")
-print(f"Projected savings after one year, with interest, is: ${projectedSavings}")
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")

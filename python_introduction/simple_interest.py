a831d95b3937d89136cbdf97c10aa05efd557b01
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2024-12-14 14:10:49 +0200

    added the second task solution

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+# simple interest formula is I = P * R * T
+principal = 1000 # $1000
+rate = 0.05 # 5% annual interest rate
+time = 3 # 3 years
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")

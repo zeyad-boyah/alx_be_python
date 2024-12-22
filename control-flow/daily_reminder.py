d790d3860c74a97ddce1bd491548ab31abb13640
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2024-12-22 23:30:45 +0200

    changed the text in daily_reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,22 +7,21 @@ def main():
         else:
             print("Invalid priority. Please enter 'high', 'medium', or 'low'.")
 
-
     time_bound = input("Is it time-bound? (yes/no): ").lower()
-
 
     match priority:
         case "high":
             reminder_message = f"Reminder: '{task}' is a high priority task"
         case "medium":
-            reminder_message = f"Note: '{task}' is a medium priority task."
+            reminder_message = f"Reminder: '{task}' is a medium priority task."
         case "low":
-            reminder_message = f"Note: '{task}' is a low priority task."
+            reminder_message = f"Reminder: '{task}' is a low priority task."
     if time_bound == "yes":
         reminder_message += " that requires immediate attention today!"
     elif time_bound == "no":
         reminder_message += " Consider completing it when you have free time."
 
     print(reminder_message)
+
 
 main()

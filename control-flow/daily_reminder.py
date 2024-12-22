ef1eea92d4704b269f8a282991aaa460796e0d51
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2024-12-22 23:36:20 +0200

    removed some eerror checking that was causing some bugs

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,15 @@
 def main():
-    task = input("Enter your task: ")
-    while True:
-        priority = input("Priority (high/medium/low): ").lower()
-        if priority in ["high", "medium", "low"]:
-            break
-        else:
-            print("Invalid priority. Please enter 'high', 'medium', or 'low'.")
-
+    task = input("Enter your task: ") 
+    priority = input("Priority (high/medium/low): ").lower()
     time_bound = input("Is it time-bound? (yes/no): ").lower()
 
     match priority:
         case "high":
             reminder_message = f"Reminder: '{task}' is a high priority task"
         case "medium":
-            reminder_message = f"Reminder: '{task}' is a medium priority task."
+            reminder_message = f"Reminder: '{task}' is a medium priority task"
         case "low":
-            reminder_message = f"Reminder: '{task}' is a low priority task."
+            reminder_message = f"Reminder: '{task}' is a low priority task"
     if time_bound == "yes":
         reminder_message += " that requires immediate attention today!"
     elif time_bound == "no":

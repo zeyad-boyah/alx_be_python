13bcf2877a97e8241624e449cad19432dc4cce72
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-12 21:41:33 +0200

    add first draft from explore_datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H-%M-%S")
+    print(formatted_date)
+
+
+def calculate_future_date():
+    num_of_days_in_the_future = int(input("please provide the number of days: "))
+    now = datetime.now()
+    future_date = now + timedelta(days=num_of_days_in_the_future)
+    print(future_date.strftime("%Y-%m-%d"))
+
+
+display_current_datetime()
+calculate_future_date()

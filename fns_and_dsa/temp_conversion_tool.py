c60e7269c5533faba43112a2d699a1b1859b201b
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-12 22:03:13 +0200

    add error handling for user input for temp

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -15,7 +15,11 @@ def convert_to_fahrenheit(celsius):
 
 
 def main():
-    temp = float(input("Enter the temperature to convert: "))
+    try:
+        temp = float(input("Enter the temperature to convert: "))
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
+
     temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
     if temp_type == "F":
         print(f"{temp}°F is {convert_to_celsius(temp)}°C")

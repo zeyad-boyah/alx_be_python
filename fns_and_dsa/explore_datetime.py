3132098a27824bb5fef83e0d0b47dcd48e74f774
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-12 21:44:10 +0200

    rephrases wording in calculate_future_date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
 
 
 def calculate_future_date():
-    num_of_days_in_the_future = int(input("please provide the number of days: "))
+    num_of_days_in_the_future = int(input("Enter the number of days to add to the current date: "))
     now = datetime.now()
     future_date = now + timedelta(days=num_of_days_in_the_future)
     print(future_date.strftime("%Y-%m-%d"))

994c3bc257506ade23c0c61857f06d9bc49eb9eb
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-12 18:02:13 +0200

    formats the codebase

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,19 @@
 ["num1, num2, operation"]
 
-def perform_operation(num1,num2,operation):
 
-    if (operation == 'divide' and num2 == 0):
+def perform_operation(num1, num2, operation):
+
+    if operation == "divide" and num2 == 0:
         return "can not divide by zero"
 
     match operation:
-        case 'add':
-            return num1+num2
-        case 'subtract':
-            return num1-num2
-        case 'multiply':
-            return num1*num1
-        case 'divide':
-            return num1/num2
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num1
+        case "divide":
+            return num1 / num2
         case _:
             return "please provide a valid operation"
-
-
-
-
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,3 +1,3 @@
 hours = 2
-seconds = hours * 3600 
-print (f"{hours} hour(s) is {seconds} seconds")
+seconds = hours * 3600
+print(f"{hours} hour(s) is {seconds} seconds")

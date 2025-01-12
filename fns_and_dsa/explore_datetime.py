9defa9a57770f6311adb2e5c77ce06d7560a0321
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-12 21:43:06 +0200

    fixes a typo in formatted_date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    formatted_date = current_date.strftime("%Y-%m-%d %H-%M-%S")
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(formatted_date)
 
 

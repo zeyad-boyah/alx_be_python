0fa5822399d37eecbea7d8e8b38c42094942eab2
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-12 18:07:47 +0200

    refactors the code to use if statment instead

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,19 +2,16 @@
 
 
 def perform_operation(num1, num2, operation):
-
     if operation == "divide" and num2 == 0:
         return "can not divide by zero"
-
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            multi = num1 * num2
-            return multi
-        case "divide":
-            return num1 / num2
-        case _:
-            return "please provide a valid operation"
+    
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2
+    else:
+        return "please provide a valid operation"

12dbe0694d6efd59167f6f0b902874d2ba5e297c
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-19 12:09:16 +0200

    add the bank_account first implementation

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        self.account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"Current Balance: {self.account_balance}")

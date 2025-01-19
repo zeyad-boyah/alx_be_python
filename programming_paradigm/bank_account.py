aae65171d6524f9abfd7563e692d128e5ddb3180
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-19 12:10:56 +0200

    add $ to the display_balance return text

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,4 +14,4 @@ class BankAccount:
         return True
 
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")

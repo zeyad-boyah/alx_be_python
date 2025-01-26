b14affe8609aa5dc87e3b78e977488325b43434d
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-26 22:01:36 +0200

    add first draft for polymorphism_emo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,28 @@
+from math import pi
+
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        super().__init__()
+        self.length = float(length)
+        self.width = float(width)
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius: float):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return pi * (self.radius**2)

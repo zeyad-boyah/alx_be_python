a2c407539f559ed1cabb8044830bbac684615bbc
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-26 22:19:18 +0200

    fixes an issue in class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,14 +1,11 @@
 class Calculator:
     calculation_type = "Arithmetic Operations"
-    def __init__(self):
-        pass
 
     @staticmethod
-    def add(a,b):
+    def add(a, b):
         return a + b
-    
+
     @classmethod
-    def multiply(cls, a ,b):
+    def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
-    

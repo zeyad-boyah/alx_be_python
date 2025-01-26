89bde548ec6896216b076c16c094d42c942fb0da
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-26 21:50:59 +0200

    fixes an issue for the IDE

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,8 +1,20 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
+
+
 class Book:
     def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-    
+
     def __str__(self):
         return f"{self.__class__.__name__}: {self.title} by {self.author}"
 
@@ -20,18 +32,6 @@ class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-    
+
     def __str__(self):
         return f"{self.__class__.__name__}: {self.title} by {self.author}, Page Count: {self.page_count}"
-
-class Library:
-    books = []
-    def __init__(self):
-        pass
-
-    def add_book(self, book):
-        Library.books.append(book)
-
-    def list_books(self):
-        for book in Library.books:
-            print(book)
